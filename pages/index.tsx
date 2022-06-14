9d19efa17b601a98e8b414ad4c5a283536174fc5
Benji <benjaminpeto23@gmail.com>
2022-06-14 14:43:56 +0200

    add fetch with static rendering, pulling posts from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,43 @@
-import type { NextPage } from "next";
-import type { ReactElement } from "react";
-import Layout from "../components/Layout";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import styles from "../styles/Layout.module.css";
 
-const Home: NextPage = () => {
+interface IPost {
+	posts: PostType[];
+}
+
+type PostType = {
+	userId: number;
+	id: string;
+	title: string;
+	body: string;
+};
+
+const Home = ({ posts }: IPost) => {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Dev News</title>
 			</Head>
-			<h1>Welcome on my blog!</h1>
+			{posts.map((post) => (
+				<h3 key={post.id}>{post.title}</h3>
+			))}
 		</div>
 	);
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=10`
+	);
+	const posts: IPost[] = await res.json();
+
+	return {
+		props: {
+			posts,
+		},
+	};
+};
